27d069d1b51d452e42563d92b8c560e3708d5239
Ke Liu <ke.liu1@thoughtworks.com>
2020-09-17 16:25:50 +0800

    [lk] add first solution

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/api/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/api/GreetingController.java
@@ -1,10 +1,16 @@
-package com.thoughtworks.capability.demospringioccontainer;
+package com.thoughtworks.capability.demospringioccontainer.api;
 
+import com.thoughtworks.capability.demospringioccontainer.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
+
 @RestController
+@Scope(SCOPE_SINGLETON)
 public class GreetingController {
 
     private final GreetingService greetingService;
@@ -12,6 +18,7 @@ public class GreetingController {
     @Autowired
     public GreetingController(GreetingService greetingService) {
         this.greetingService = greetingService;
+        System.out.println("**********************");
     }
 
     @GetMapping("/greet")
---
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/service/GreetingService.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.capability.demospringioccontainer.service;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Service;
+
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
+
+@Service
+//@Scope(value = SCOPE_SINGLETON,proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Scope(value = SCOPE_PROTOTYPE,proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class GreetingService {
+   public GreetingService(){
+
+           System.out.println("################");
+
+   }
+   public String sayHi() {
+        return "hello world";
+    }
+}
